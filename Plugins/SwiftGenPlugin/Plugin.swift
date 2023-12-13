f2e558ae51de3ccce25fd8d4b2ca476999c2496e
Zach Radford <zach.radford@faire.com>
2023-12-12 22:33:40 -0500

    apply changes

--- Plugins/SwiftGenPlugin/Plugin.swift
+++ Plugins/SwiftGenPlugin/Plugin.swift
@@ -4,6 +4,7 @@
 // MIT Licence
 //
 
+import CryptoKit
 import Foundation
 import PackagePlugin
 
@@ -22,6 +23,41 @@ struct SwiftGenPlugin: BuildToolPlugin {
       return []
     }
 
+     let checksum = combinedChecksum(for: configurations)
+     if let checksum = checksum,
+        let oldChecksum = try? String(contentsOfFile: context.checksumPath.string),
+        oldChecksum == checksum {
+     } else {
+       // Clear the SwiftGen plugin's directory (in case of dangling files)
+       Diagnostics.remark("It seems like the `swiftgen.yml` files have changed, removing cached data")
+       fileManager.forceClean(directory: context.pluginWorkDirectory)
+     }
+     try? checksum?.write(toFile: context.checksumPath.string, atomically: true, encoding: .utf8)
+
+     try? fileManager.createDirectory(atPath: context.outputDirectoryPath.string, withIntermediateDirectories: true)
+    return try configurations.map { configuration in
+      try .swiftgen(using: configuration, context: context, target: target)
+    }
+  }
+}
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension SwiftGenPlugin: XcodeBuildToolPlugin {
+  func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+    let fileManager = FileManager.default
+
+    // Possible paths where there may be a config file (root of package, target dir.)
+    let configurations: [Path] = [context.xcodeProject.directory]
+      .map { $0.appending("swiftgen.yml") }
+      .filter { fileManager.fileExists(atPath: $0.string) }
+
+    // Validate paths list
+    guard validate(configurations: configurations, target: target) else {
+      return []
+    }
+
     // Clear the SwiftGen plugin's directory (in case of dangling files)
     fileManager.forceClean(directory: context.pluginWorkDirectory)
 
@@ -30,6 +66,7 @@ struct SwiftGenPlugin: BuildToolPlugin {
     }
   }
 }
+#endif
 
 // MARK: - Helpers
 
@@ -47,6 +84,21 @@ private extension SwiftGenPlugin {
 
     return true
   }
+
+#if canImport(XcodeProjectPlugin)
+  func validate(configurations: [Path], target: XcodeTarget) -> Bool {
+    guard !configurations.isEmpty else {
+      Diagnostics.error("""
+        No SwiftGen configurations found for target \(target.displayName). If you would like to generate sources for this \
+        target include a `swiftgen.yml` in the target's source directory, or include a shared `swiftgen.yml` at the \
+        package's root.
+        """)
+      return false
+    }
+
+    return true
+  }
+#endif
 }
 
 private extension Command {
@@ -64,11 +116,37 @@ private extension Command {
         "PROJECT_DIR": context.package.directory,
         "TARGET_NAME": target.name,
         "PRODUCT_MODULE_NAME": target.moduleName,
+        "DERIVED_SOURCES_DIR": context.outputDirectoryPath
+      ],
+      outputFilesDirectory: context.outputDirectoryPath
+    )
+  }
+
+#if canImport(XcodeProjectPlugin)
+  static func swiftgen(using configuration: Path, context: XcodePluginContext, target: XcodeTarget) throws -> Command {
+    .prebuildCommand(
+      displayName: "SwiftGen BuildTool Plugin",
+      executable: try context.tool(named: "swiftgen").path,
+      arguments: [
+        "config",
+        "run",
+        "--verbose",
+        "--config", "\(configuration)"
+      ],
+      environment: [
+        "PROJECT_DIR": context.xcodeProject.directory,
+        "TARGET_NAME": target.displayName,
         "DERIVED_SOURCES_DIR": context.pluginWorkDirectory
       ],
       outputFilesDirectory: context.pluginWorkDirectory
     )
   }
+#endif
+}
+
+private extension PluginContext {
+    var outputDirectoryPath: Path { pluginWorkDirectory.appending(subpath: "output") }
+    var checksumPath: Path { pluginWorkDirectory.appending(subpath: "swiftgen-checksums") }
 }
 
 private extension FileManager {
@@ -90,4 +168,8 @@ extension Target {
       return ""
     }
   }
+}
+
+private func combinedChecksum(for configurations: [Path]) -> String? {
+    try? configurations.map { Insecure.MD5.hash(data: try Data(contentsOf: URL(fileURLWithPath: $0.string))).description }.joined(separator: "-")
 }
